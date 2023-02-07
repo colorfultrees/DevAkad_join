a4a9e8d6d9b0426f17b16babd17d8fa15c9df05b
colorfultrees <colorfultrees@users.noreply.github.com>
2023-02-07 18:51:43 +0100

    div closed _contact-div-big_

--- js/contacts_html.js
+++ js/contacts_html.js
@@ -24,25 +24,26 @@ function contactDivHTML(index) {
 function contactCardHTML(index) {
     return /*html*/ `
         <div class="contact-div-big">
-        <div class="username-initials-big" id="u-initials${index}">JM</div>
-            <div>
-                <div class="username-big" id="u-name${index}">Hans Test</div>
-                <img onclick="openAddTaskContact(${index})" src="./assets/img/add_task.png">
+            <div class="username-initials-big" id="u-initials${index}">JM</div>
+                <div>
+                    <div class="username-big" id="u-name${index}">Hans Test</div>
+                    <img onclick="openAddTaskContact(${index})" src="./assets/img/add_task.png">
+                </div>
             </div>
-        </div>
-        <div class="contact-and-edit">
-            <div class="contact-information">Contact Information</div>
-            <div id="edit-contact" class="edit-contact"><img src="./assets/img/edit_contact.png"><span>Edit Contact</span></div>
-        </div>
-        <div id="delete-contact-container" onclick="requestDelete('request-delete-contact-popup')">
-            <div id="delete-contact" class="edit-contact">
-                <img src="./assets/img/delete.png">
-                <span id="delete-contact-text">Delete Contact</span>
+            <div class="contact-and-edit">
+                <div class="contact-information">Contact Information</div>
+                <div id="edit-contact" class="edit-contact"><img src="./assets/img/edit_contact.png"><span>Edit Contact</span></div>
             </div>
+            <div id="delete-contact-container" onclick="requestDelete('request-delete-contact-popup')">
+                <div id="delete-contact" class="edit-contact">
+                    <img src="./assets/img/delete.png">
+                    <span id="delete-contact-text">Delete Contact</span>
+                </div>
+            </div>
+            <div class="email-margins"><b>Email</b></div>
+            <a class="users-email-big" id="u-email${index}">testmail</a>
+            <div class="phone-margins"><b>Phone</b></div>
+            <div id="u-phone-number${index}">no phone number</div>
         </div>
-        <div class="email-margins"><b>Email</b></div>
-        <a class="users-email-big" id="u-email${index}">testmail</a>
-        <div class="phone-margins"><b>Phone</b></div>
-        <div id="u-phone-number${index}">no phone number</div>
     `;
 }
