d9692c2b69b0dfe8cdd61286d72fcee01e2cd27a
colorfultrees <colorfultrees@users.noreply.github.com>
2023-02-08 19:25:33 +0100

    bugfix _contact form validation ignored_

--- js/contacts_modal.js
+++ js/contacts_modal.js
@@ -201,15 +201,17 @@ function setBadgeEditor(id) {
  */
 function setEditorButtons(id) {
     const btnsNew = document.getElementById('contact-submit-btns');
-    const createBtn = document.getElementById('contact-create-btn');
+    // const createBtn = document.getElementById('contact-create-btn');
     const containerSave = document.getElementById('contact-save-btn');
-    const btnSave = document.getElementById('contact-update-btn');
+    // const btnSave = document.getElementById('contact-update-btn');
+    const contactForm = document.getElementById('contact-form');
     btnsNew.classList.add('d-none');
-    createBtn.removeAttribute('onclick');
-    createBtn.removeAttribute('type');
+    // createBtn.removeAttribute('onclick');
+    // createBtn.removeAttribute('type');
     containerSave.classList.remove('d-none');
-    btnSave.setAttribute('onclick', `updateContact(${id})`);
-    btnSave.setAttribute('type', 'submit');
+    // btnSave.setAttribute('onclick', `updateContact(${id})`);
+    // btnSave.setAttribute('type', 'submit');
+    contactForm.setAttribute('onsubmit', `updateContact(${id}); return false;`)
 }
 
 
@@ -218,15 +220,17 @@ function setEditorButtons(id) {
  */
 function setContactFormButtons() {
     const btnsNew = document.getElementById('contact-submit-btns');
-    const createBtn = document.getElementById('contact-create-btn');
+    // const createBtn = document.getElementById('contact-create-btn');
     const containerSave = document.getElementById('contact-save-btn');
-    const btnSave = document.getElementById('contact-update-btn');
+    // const btnSave = document.getElementById('contact-update-btn');
+    const contactForm = document.getElementById('contact-form');
     btnsNew.classList.remove('d-none');
-    createBtn.setAttribute('onclick', 'saveNewContact()');
-    createBtn.setAttribute('type', 'submit');
+    // createBtn.setAttribute('onclick', 'saveNewContact()');
+    // createBtn.setAttribute('type', 'submit');
     containerSave.classList.add('d-none');
-    btnSave.removeAttribute('onclick');
-    btnSave.removeAttribute('type');
+    // btnSave.removeAttribute('onclick');
+    // btnSave.removeAttribute('type');
+    contactForm.setAttribute('onsubmit', `saveNewContact(); return false;`)
 }
 
 
