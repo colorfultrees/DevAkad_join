ac138784fbec186fa9ebe5687fd20e2541d9733d
colorfultrees <colorfultrees@users.noreply.github.com>
2023-05-07 16:46:37 +0200

    reset pw _msg reworked

--- send_mail.php
+++ send_mail.php
@@ -41,14 +41,15 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
         $email = $_POST['email'];
         $message = "Hello,\n
-        \nFollow this link to reset your JOIN password for your account:
+        \nyou have made a request to reset your JOIN password.\n
+        \nTo proceed please follow this link:
         \nhttps://join.christof-mark.com/reset-password.html?email=".$email."\n
         \nYour JOIN team!";
 
         $recipient = $email;
 
         $subject = "Reset your JOIN password";
-        $headers = "From: noreply@christof-mark.com";
+        $headers = "From: noreply@join-team.com";
 
         $result = mail($recipient, $subject, $message, $headers);
         print($result);
